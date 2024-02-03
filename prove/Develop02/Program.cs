fffd71e64dac57ba5680bc5238bdb0b8c1ab5e3b
cs-oliver <cameron.s.oliver@gmail.com>
2024-02-03 22:25:26 +0100

    Working on Developer 2

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,60 @@
 using System;
 
-class Program
+namespace DailyJournal
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Develop02 World!");
+        static void Main(string[] args)
+        {
+            Journal journal = new Journal();
+
+            List<string> prompts = Prompt.GetPrompts();
+
+            bool quit = false;
+            while (!quit)
+            {
+                Console.WriteLine("\nWelcome to the Journal Program");
+                Console.WriteLine("Please Select one of the following choices:");
+                Console.WriteLine("1. Write");
+                Console.WriteLine("2. Display");
+                Console.WriteLine("3. Save");
+                Console.WriteLine("4. Load");
+                Console.WriteLine("5. Quit");
+
+                Console.WriteLine("\nWhat would you like to do? ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        string prompt = GetRandomPrompt(prompts);
+                        journal.AddEntry(prompt);
+                        break;
+                    case "2":
+                        journal.DisplayEntries();
+                        break;
+                    case "3":
+                        journal.SaveToFile();
+                        break;
+                    case "4":
+                        journal.LoadFromFile();
+                        break;
+                    case "5":
+                        quit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+            }
+            Console.WriteLine("Until next time, keep writing!");
+        }
+
+        static string GetRandomPrompt(List<string> prompts)
+        {
+            Random random = new Random();
+            int index = random.Next(prompts.Count);
+            return prompts[index];
+        }
     }
 }
---
+++ prove/Develop02/Prompt.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace DailyJournal
+{
+    public static class Prompt
+    {
+        public static List<string> GetPrompts()
+        {
+
+            return new List<string>
+            {
+                "Who was the most interesting person I interacted with today?",
+                "What was the best part of my day?",
+                "How did I see the hand of the Lord in my life today?",
+                "What was the strongest emotion I felt today?",
+                "If I had one thing I could do over today, what would it be?",
+                "What are some habits I want to cultivate or break, and how can I make progress towards those goals?",
+                "Who do you trust most? Why?",
+                "What are your strengths in relationships (kindness, empathy, etc.)?",
+                "How do you draw strength from loved ones?",
+                "Did you show any kindness today?",
+                "What do you value most in relationships (trust, respect, sense of humor, etc.)?",
+                "What three important things have you learned from previous relationships?",
+                "What five traits do you value most in potential partners?",
+                "How do you show compassion to others? How can you extend that same compassion to yourself?",
+                "What are three things working well in your current relationship? What are three things that could be better?",
+                "What boundaries could you set in your relationships to safeguard your own well-being?",
+                "What kept you going today?",
+                "What do you most want your children (or future children) to learn from you?",
+                "How can you better support and appreciate your loved ones?",
+                "What does love mean to you? How do you recognize it in a relationship?",
+                "List three things you would like to tell a friend, family member, or partner.",
+                "How do you use your personal strengths and abilities at work?",
+                "How do your co-workers and supervisors recognize your strengths?",
+                "How does work fulfill you? Does it leave you wanting more?",
+                "What part of your workday do you most enjoy?",
+                "What about your work feels real, necessary, or important to you?",
+                "Do you see yourself in the same job in 10 years?",
+                "What made you smile today?",
+                "What are your career ambitions?",
+                "What three things can help you begin working to accomplish those goals?",
+                "What can you do to improve your work performance?",
+                "What does your work teach you? Does it offer continued opportunities for learning and growth?",
+                "Does your work drain or overwhelm you? Why? Is this something you can change?"
+            };
+        }
+    }
+}
+
